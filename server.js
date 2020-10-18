ebf3e2b862f4166306fa3589cb71ae39a010e4b2
Luke <business.luked@gmail.com>
2020-10-17 21:01:18 -0400

    Express base

---
+++ server.js
@@ -0,0 +1,15 @@
+
+// set up express server
+var express = require("express");
+var app = express();
+var port = process.env.PORT || '4334';
+var server = app.listen(port);
+
+console.log("Server: " + port);
+
+app.get('/', (req,res) => {
+    res.send('Welcome to our Smartboard.');
+});
+
+
+exports.server = server;
