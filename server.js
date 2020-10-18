132f698e4fe64b8f75e701a50149d75f6142d96a
Peter Zheng <scholarpetez@gmail.com>
2020-10-18 00:51:34 -0400

    Implementation of Express backend and sockets.

--- server.js
+++ server.js
@@ -2,17 +2,64 @@
 // set up express server
 var express = require("express");
 var app = express();
-var port = process.env.PORT || '4334';
-var server = app.listen(port);
+var portNum = process.env.PORT || '5000';
+var server = app.listen(portNum);
+app.use(express.static("public"));
 
-console.log("Server: " + port);
+console.log("server running on port: " + portNum);
+
+// set up socket.io on express server
+var io = require("socket.io")(server);
 
 app.get('/', (req,res) => {
-    res.send('Welcome to our Smartboard.');
+    console.log("Test");
+    res.send('Welcome to Chalkboard');
 });
 
-app.get('/randomroute', (req,res) => {
-    res.send('Welcome to our Smartboard.');
+io.on('connection', (socket) => {
+    console.log("new connection: " + socket.id);
+    socket.send("hello");
+
+
+    let name = "Guest" +  Math.floor( Math.random() * 10000);
+
+    socket.on('startPos', (data) => {
+        socket.broadcast.emit('lock');        // broadcast to all sockets except sender who triggered event
+        console.log("Start position");
+        io.emit('startPos', data);            // broadcast to all sockets, including sender who triggered event
+    });
+
+    socket.on('mousePos', (data) => {
+        socket.broadcast.emit('lock');
+        console.log("Start position");
+        io.emit('mousePos', data);
+    });
+
+    socket.on('finishPos', () => {
+        console.log("Start position");
+
+        io.emit('finishPos');
+    });
+
+    socket.on("sentMessage", (message) => {
+        socket.broadcast.emit(message);
+        console.log("Start position");
+
+    });
+
+    // chat handling
+
+    socket.on("sendChatMessage", (message) => {
+        let time = new Date();
+        let formattedTime = time.toLocaleString("en-US", {hour: "numeric", minute: "numeric"});
+        io.emit("chat-message", name + " at " + formattedTime + ":\n" + message);
+    });
+
+
+    socket.on("typingMsg", (data) => {
+        socket.broadcast.emit("typing", name, data);
+    });
+
 });
 
 
